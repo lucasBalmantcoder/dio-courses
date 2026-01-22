79f57100c77993c1853f28bb600959596c047f81
Lucas Lopes <130125852+lucasBalmantcoder@users.noreply.github.com>
2026-01-21 23:50:51 -0300

    finalização-de-atividade

--- formação-nodejs-fundamental/09-npm-enviroment/src/database/data.js
+++ formação-nodejs-fundamental/09-npm-enviroment/src/database/data.js
@@ -1,5 +1,5 @@
 async function conectToDataBase(user, password) {
-    if (user === 'pass' && password === 'pass') {
+    if (user === process.env.USERDATABASE && password === process.env.PASSWORDDATABASE) {
          console.log("conexão com o db estabelecida");         
     }else{
         console.log("falha na conexão com o db");
--- formação-nodejs-fundamental/09-npm-enviroment/src/index.js
+++ formação-nodejs-fundamental/09-npm-enviroment/src/index.js
@@ -2,6 +2,8 @@ import { conectToDataBase } from "./database/data.js";
 
 async function main() {
     await conectToDataBase("pass","pass")
+    // console.log(process.env.USERDATABASE);
+    
 }
 
 main();
