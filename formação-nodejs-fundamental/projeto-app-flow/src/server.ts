35b1ddb93112003efd32c398465b9168f3190da9
Lucas Lopes <130125852+lucasBalmantcoder@users.noreply.github.com>
2026-01-23 23:23:31 -0300

    fet: criando a o server

---
+++ formação-nodejs-fundamental/projeto-app-flow/src/server.ts
@@ -0,0 +1,13 @@
+import * as http from "http";
+
+
+const server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {
+        
+    }
+);
+
+const port = process.env.PORT
+
+
+server.listen(port, ()=> console.log(`Server is running on http://localhost:${port}`));
