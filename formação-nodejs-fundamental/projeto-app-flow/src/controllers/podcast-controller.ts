d539d5325e492da53d5419274b6acc72ee03c9f9
Lucas Lopes <130125852+lucasBalmantcoder@users.noreply.github.com>
2026-01-25 15:13:27 -0300

    fet: criação-de-rotas

--- formação-nodejs-fundamental/projeto-app-flow/src/controllers/podcast-controller.ts
+++ formação-nodejs-fundamental/projeto-app-flow/src/controllers/podcast-controller.ts
@@ -1,29 +1,36 @@
 import {IncomingMessage, request, ServerResponse} from "http";
 
+import { serviceFilterEpisodes } from "../services/filter-episodes-service";
+import { servieListEpisodes } from "../services/list-episodes-service";
 
 
-export const getListEpisodes =  async (request: IncomingMessage, response: ServerResponse) => {
+
+
+export const getListEpisodes =  async (
+    request: IncomingMessage, 
+    response: ServerResponse
+) => {
+
+    const content = await servieListEpisodes();
+
+    response.writeHead(200, {
+        'Content-Type': 'application/json'});
+
+    response.end(JSON.stringify(content));
+
+
+
+}
+
+export const getFilterEpisodes = async (
+    request: IncomingMessage, 
+    response: ServerResponse
+) => {
+    const content = await serviceFilterEpisodes("flow");
+
     response.writeHead(200, {
         'Content-Type': 'application/json'
     });
 
-    response.end(
-        JSON.stringify([
-            {
-            podcastName: "flow",
-            episode: "CBUM - Flow #319",
-            videoId: "pQSuQmUfS30",
-            categories: ["saúde", "esporte", "bodybuilder"]
-            },
-            {
-            podcastName: "flow",
-            episode: "RUBENS BARRICHELLO - Flow #339",
-            videoId: "4KDGTdiOV4I",
-            categories: ["esporte", "corrida"]
-            }
-        ])
-);
-
-    
-
+    response.end(JSON.stringify(content));
 }
--- formação-nodejs-fundamental/projeto-app-flow/src/server.ts
+++ formação-nodejs-fundamental/projeto-app-flow/src/server.ts
@@ -1,12 +1,18 @@
 import * as http from "http";
-import {getListEpisodes} from "./controllers/podcast-controller";
+import {getFilterEpisodes, getListEpisodes} from "./controllers/podcast-controller";
 
 
 
 const server = http.createServer(
     async (request: http.IncomingMessage, response: http.ServerResponse) => {
-        if (request.method === 'GET') {
+
+
+        // listar podcast
+        if (request.method === 'GET' && request.url === '/api/list') {
             await getListEpisodes(request, response);
+        }
+        if (request.method === 'GET' && request.url === '/api/episodes') {
+            await getFilterEpisodes(request, response);
         }
     }
 );
@@ -15,3 +21,4 @@ const port = process.env.PORT
 
 
 server.listen(port, ()=> console.log(`Server is running on http://localhost:${port}`));
+
