82761e59e4d2ca28d7ccdc84ffc7e6572ce13089
Lucas Lopes <130125852+lucasBalmantcoder@users.noreply.github.com>
2026-01-26 22:58:08 -0300

    new-project-node-f1

---
+++ formação-nodejs-fundamental/node-f1/src/server.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+
+
+const server = fastify({
+    logger: true
+});
+
+server.get("/teams", async (request, response) => {
+    response.type("application/json").code(200);
+
+    return[{id: 1, name: "ferrari"}];
+});
+
+
+server.listen({port:3333}, () => {
+    console.log("server init");
+});
+
